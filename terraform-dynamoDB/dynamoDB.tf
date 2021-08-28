08474c9eca1eb7abc284c69cc5031125b82f59f6
Vitor Fred <vitor.fred@gmail.com>
2021-08-28 00:53:13 -0300

    Update dynamoDB.tf

--- terraform-dynamoDB/dynamoDB.tf
+++ terraform-dynamoDB/dynamoDB.tf
@@ -4,21 +4,18 @@ provider "aws" {
 
 resource "aws_dynamodb_table" "dynamoDB" {
   name   = "cliente"
-#  billing_mode   = "PROVISIONED"
+  billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
   hash_key   = "codigo"
-  range_key  = "nome"
+  #range_key  = "nome"
   
   attribute {
     name = "codigo"
     type = "S"
   }  
   
-  attribute {
-    name = "nome"
-    type = "S"
-  }  
+
   
   ttl {
     attribute_name = "TimeToExist"
@@ -26,22 +23,6 @@ resource "aws_dynamodb_table" "dynamoDB" {
   }
 
 
- #local_secondary_indexes = [{
- #   name               = "customerId-city-index"
- #   range_key          = "city"
- #   projection_type    = "ALL"                   # ALL, INCLUDE or KEYS_ONLY
- #   non_key_attributes = []
- # }]
-  
-#  global_secondary_index {
-#    name               = "GameTitleIndex"
-#    hash_key           = "GameTitle"
-#    range_key          = "TopScore"
-#    write_capacity     = 10
-#    read_capacity      = 10
-#    projection_type    = "INCLUDE"
-#    non_key_attributes = ["UserId"]
-#  }
 
   tags = {
     Name        = "dynamodb-lab-senai"
